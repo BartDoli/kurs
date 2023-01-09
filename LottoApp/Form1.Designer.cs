b1c5bb47d49be47c3dabef848e192dd3dabf8605
BartDoli <qq118673@gmail.com>
2023-01-10 00:41:02 +0100

    Mayor changes

---
+++ LottoApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LottoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.WinningNumbersBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(537, 411);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter 6-digit numbers, separated by \" ,\"";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(628, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run lotto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WinningNumbersBox
+            // 
+            this.WinningNumbersBox.Location = new System.Drawing.Point(555, 27);
+            this.WinningNumbersBox.Name = "WinningNumbersBox";
+            this.WinningNumbersBox.ReadOnly = true;
+            this.WinningNumbersBox.Size = new System.Drawing.Size(233, 23);
+            this.WinningNumbersBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(555, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Winning numbers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WinningNumbersBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Lotto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private TextBox WinningNumbersBox;
+        private Label label2;
+    }
+}
---
+++ LottoApp/Form1.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace LottoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int num = random.Next(100000, 999999);
+            string numString = num.ToString().PadLeft(6, '0');
+
+            string userInput = textBox1.Text;
+            string[] numStringList = numString.Split(" , ");
+
+            if (userInput.Count() < 6)
+            {
+                MessageBox.Show("Invalid Input");
+            }
+            else if (numString.Count() > 6)
+            {
+                MessageBox.Show("Invalid Input");
+            }
+            else
+            {
+                foreach (string numStr in numStringList)
+                {
+                    if (numStr == userInput)
+                    {
+                        MessageBox.Show("The numbers match!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("The numbers do not match.");
+                    }
+                    WinningNumbersBox.Text = num.ToString();
+                }
+            }
+        }
+    }
+}
